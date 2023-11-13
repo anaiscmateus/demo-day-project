29779b1dcf96a4e1bf4cedbef1a333cf8326e63b
Anais Mateus <75923327+anaiscmateus@users.noreply.github.com>
2023-11-13 03:57:58 -0500

    frontend, login auth

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://anaismateusc:0XzV5LUDFehDvEfT@tradevault.zhwr0ix.mongodb.net/tradevault?retryWrites=true&w=majority', 
+    'dbName': 'tradevault'
+};
+
